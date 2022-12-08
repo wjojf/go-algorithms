be70b7ef10bd0eeb48c1529efe2c1405fc208b47
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-12-09 01:46:58 +0500

    Update 113_path_sum_ii.go

--- 113_path_sum_ii.go
+++ 113_path_sum_ii.go
@@ -11,14 +11,14 @@ import "fmt"
 func pathSum(root *TreeNode, targetSum int) [][]int {
     var res [][]int
     var path []int
-    dfs(root, targetSum, &path, 0, &res)
+    dfs(root, targetSum, path, 0, &res)
     return res
 }
 
 func dfs(
     root *TreeNode,
     targetSum int,
-    path *[]int,
+    path []int,
     currSum int,
     res *[][] int){
     
@@ -29,17 +29,14 @@ func dfs(
 
     if root.Left == nil && root.Right == nil {
         if currSum + root.Val == targetSum {
-            f_p := append(*path, root.Val)
-            fmt.Println(f_p)
-            *res = append(*res, f_p)
-            fmt.Println(*res)
+            path = append(path, root.Val)
+            *res = append(*res, path) // append main variable 
             return
         }
+    } else {
+        c_s := currSum + root.Val
+        path = append(path, root.Val)
+        dfs(root.Left, targetSum, path, c_s, res)
+        dfs(root.Right, targetSum, path, c_s, res)
     }
-
-    c_s := currSum + root.Val
-    p := append(*path, root.Val)
-
-    dfs(root.Left, targetSum, &p, c_s, res)
-    dfs(root.Right, targetSum, &p, c_s, res)
 }
