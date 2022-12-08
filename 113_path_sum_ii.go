f81ce9522cd105418b20db316186f6b8c9674095
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-12-09 01:58:20 +0500

    Update 113_path_sum_ii.go

--- 113_path_sum_ii.go
+++ 113_path_sum_ii.go
@@ -1,11 +1,10 @@
 /**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
  */
+package main
+
 import "fmt"
 
 func pathSum(root *TreeNode, targetSum int) [][]int {
