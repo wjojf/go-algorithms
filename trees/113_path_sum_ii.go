da159bf5ed850d0adde954d9ea088d8115ac64d8
wjojf <STPKBFF@yandex.ru>
2022-12-09 15:08:49 +0500

    Add 73

--- trees/113_path_sum_ii.go
+++ trees/113_path_sum_ii.go
@@ -18,7 +18,7 @@ func pathSum(root *TreeNode, targetSum int) [][]int {
 func dfs(
     root *TreeNode,
     targetSum int,
-    path *[]int,
+    path []int,
     currSum int,
     res *[][] int){
     
@@ -29,7 +29,7 @@ func dfs(
 
     if root.Left == nil && root.Right == nil {
         if currSum + root.Val == targetSum {
-            f_p := append(*path, root.Val)
+            f_p := append(path, root.Val)
             fmt.Println(f_p)
             *res = append(*res, f_p)
             fmt.Println(*res)
@@ -38,8 +38,8 @@ func dfs(
     }
 
     c_s := currSum + root.Val
-    p := append(*path, root.Val)
+    p := append(path, root.Val)
 
-    dfs(root.Left, targetSum, &p, c_s, res)
-    dfs(root.Right, targetSum, &p, c_s, res)
+    dfs(root.Left, targetSum, p, c_s, res)
+    dfs(root.Right, targetSum, p, c_s, res)
 }
